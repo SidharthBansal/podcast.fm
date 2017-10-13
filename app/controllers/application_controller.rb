7d5bef90f84eb3458caf3c7e17c1656285b84fce
Sidharth <bansal.sidharth2996@gmail.com>
2017-10-14 00:50:48 +0530

    Added paperclip gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.permit(:sign_up){|u| u.permit(:email,:password, :password_confirmation,:title )}
-  	devise_parameter_sanitizer.permit(:account_update){ |u| u.permit(:email, :password, :password_confirmation, :current_password, :title, :description, :itunes, :stitcher, "podboy")}
+  	devise_parameter_sanitizer.permit(:sign_up){|u| u.permit(:email,:password, :password_confirmation,:title , :thumbnail)}
+  	devise_parameter_sanitizer.permit(:account_update){ |u| u.permit(:email, :password, :password_confirmation, :current_password, :title, :thumbnail, :description, :itunes, :stitcher, :podboy)}
   end
 end
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -4,4 +4,8 @@ class Podcast < ApplicationRecord
   has_many :episodes
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  	has_attached_file :thumbnail, styles: { large: "1000x1000#", medium: "550x550#" }
+	validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\z/
+
+
 end
